2f77c0dab8a72e7e0af2cd4666f23da2cc004b78
Pavel07-chyden <pates1993@mail.ru>
2022-02-05 19:42:25 +0300

    commit pages

--- src/pages/Cards/CardsGradeRange/CardsGradeRange.tsx
+++ src/pages/Cards/CardsGradeRange/CardsGradeRange.tsx
@@ -1,21 +1,21 @@
-import React, {FC, useCallback, useEffect, useState} from 'react'
-import {Range} from 'rc-slider'
+import React, { FC, useCallback, useEffect, useState } from 'react'
+import { Range } from 'rc-slider'
 import _ from 'lodash'
-import {setCurrentGrade, setMinMaxGrade} from '../../../store/reducers/cards-reducer'
-import {useDispatch} from 'react-redux'
+import { setCurrentGrade, setMinMaxGrade } from '../../../store/reducers/cards-reducer'
+import { useDispatch } from 'react-redux'
 
 type CardsGradeRangeProps = {
     minGrade: number
     maxGrade: number
 }
 
-export const CardsGradeRange: FC<CardsGradeRangeProps> = ({minGrade, maxGrade}) => {
+export const CardsGradeRange: FC<CardsGradeRangeProps> = ({ minGrade, maxGrade }) => {
     const dispatch = useDispatch()
     const [rangeValues, setRangeValues] = useState([minGrade, maxGrade])
 
     const rangeMarks = {
-        [minGrade]: {style: {fontSize: 16}, label: minGrade},
-        [maxGrade]: {style: {fontSize: 16}, label: maxGrade}
+        [minGrade]: { style: { fontSize: 16 }, label: minGrade },
+        [maxGrade]: { style: { fontSize: 16 }, label: maxGrade }
     }
 
     const onRangeChangeHandler = (values: number[]) => {
@@ -23,7 +23,7 @@ export const CardsGradeRange: FC<CardsGradeRangeProps> = ({minGrade, maxGrade}) 
         debouncedRange(values)
     }
 
-    const debouncedRange = useCallback(_.debounce(values => dispatch(setCurrentGrade({values: values})), 500), [])
+    const debouncedRange = useCallback(_.debounce(values => dispatch(setCurrentGrade({ values: values })), 500), [])
 
     useEffect(() => {
         setRangeValues([minGrade, maxGrade])
@@ -31,10 +31,10 @@ export const CardsGradeRange: FC<CardsGradeRangeProps> = ({minGrade, maxGrade}) 
 
     return (
         <Range value={rangeValues}
-               marks={rangeMarks}
-               min={minGrade}
-               max={maxGrade}
-               onChange={onRangeChangeHandler}
-               style={{margin: '32px 8px 48px 8px', width: 'inherit'}}/>
+            marks={rangeMarks}
+            min={minGrade}
+            max={maxGrade}
+            onChange={onRangeChangeHandler}
+            style={{ margin: '32px 8px 48px 8px', width: 'inherit' }} />
     )
 }
